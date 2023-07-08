359cbf08002d737dc41996bafdaecd80aee4069b
Yoshihiro Tanaka <contact@cordea.jp>
2023-07-08 12:01:10 +0900

    bundle the css file

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,7 +1,7 @@
 import * as esbuild from "esbuild";
 
 await esbuild.build({
-  entryPoints: ["index.ts"],
+  entryPoints: ["index.ts", "index.css"],
   outdir: "dist",
   bundle: true,
   minify: true,
